5c5f09cbbc3770646db4355721ead99f65c055d0
wim_kielen@hotmail.com <wim_kielen@hotmail.com>
2020-04-22 19:41:51 +0200

    Home pagina gedaan

--- src/app/my-pages/home/home.component.ts
+++ src/app/my-pages/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ParentComponent } from 'src/app/shared/components/parent.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BewonersService, BewonerItem } from 'src/app/services/bewoners.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends ParentComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    protected snackBar: MatSnackBar,
+    protected bewonersService: BewonersService) {
+    super(snackBar) 
+  }
+  displayedColumns: string[] = ['Nr', 'Naam'];
+  dataSource = new MatTableDataSource<BewonerItem>();
 
   ngOnInit(): void {
+    let sub = this.bewonersService.getNrAndNaam$()
+      .subscribe((data:Array<BewonerItem>) => {
+        this.dataSource.data = data;
+      });
+    this.registerSubscription(sub);
   }
 
 }
